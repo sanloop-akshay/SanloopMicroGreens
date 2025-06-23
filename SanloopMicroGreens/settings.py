f66eeb509fde273bfe491fc296033251b2375cae
sanloop-akshay <akshaypranavb@gmail.com>
2025-06-23 23:55:55 +0530

    Intiated for Facebook Auth

--- SanloopMicroGreens/settings.py
+++ SanloopMicroGreens/settings.py
@@ -76,7 +76,8 @@ DATABASES = {
 }
 
 AUTHENTICATION_BACKENDS = (
-    'social_core.backends.google.GoogleOAuth2',  # Add Google backend
+    'social_core.backends.facebook.FacebookOAuth2',
+    'social_core.backends.google.GoogleOAuth2',      
     'django.contrib.auth.backends.ModelBackend',
 )
 
